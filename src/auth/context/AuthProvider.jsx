b5a77ce943d2045ce73237610cfd58c92e3c204e
Victor Cancinos <77cancinos@gmail.com>
2023-05-04 19:20:20 -0600

    first commit

---
+++ src/auth/context/AuthProvider.jsx
@@ -0,0 +1,47 @@
+//Sirve para poveer la info a toda la app
+
+import { useReducer } from "react";
+import { AuthContext } from "./AuthContext";
+import { authReducer } from "./authReducer";
+import { types } from "../types/types";
+
+const init = () => {
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    return {
+        logged: !!user,
+        user: user
+    }
+}
+
+export const AuthProvider = ({ children }) => {
+
+    //Reducer maneja los estados para el control
+    const [authState, dispatch] = useReducer(authReducer, { }, init);
+
+
+    const login = (name = '') => {
+        const user = { id: 'ABC', name}
+        const action = {type: types.login, payload: user }
+        localStorage.setItem('user', JSON.stringify(user));
+        dispatch(action);
+    }
+
+    const logout = () => {
+        localStorage.removeItem('user');
+        const action = {type: types.logout}
+        dispatch(action);
+    }
+
+    return (
+        <AuthContext.Provider value={{
+            ...authState,
+
+            //Methods
+            login,
+            logout
+        }}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
